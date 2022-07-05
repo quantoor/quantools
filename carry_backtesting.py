745466f8adf9b8138fef1cc9e744747ed1fc06e6
Emanuele Bolognesi <emanuele.bolognesi@nexo.io>
2022-07-05 20:27:58 +0300

    added position pnl

--- carry_backtesting.py
+++ carry_backtesting.py
@@ -7,8 +7,8 @@ import numpy as np
 api_key = 'ZTNWpAc4SsCV4nEICM6nwASP4ao7nHYvLSFzXunj'
 api_secret = 'x9tq4yIA27jF83bacZvg-uuFB6Ov6h4n4Ot672QI'
 
-COIN = 'AAVE'
-TRADE_AMOUNT = 100  # usd
+COIN = '1INCH'
+TRADE_AMOUNT = 1000  # usd
 MIN_THRESHOLD = 0.1  # %
 MAX_THRESHOLD = 1  # %
 
@@ -26,17 +26,25 @@ class Position:
         return f'Entry price: {self.entry_price}, size: {self.size}'
 
     def update(self, price: float, size: float):
-        self.entry_price = (self.entry_price * self.size + price * size) / (self.size + size)
+        if self.entry_price is None:
+            self.entry_price = price
+        else:
+            self.entry_price = (self.entry_price * self.size + price * size) / (self.size + size)
         self.size += size
+
+    def reset(self):
+        self.entry_price = None
+        self.size = 0
 
     def _get_roe(self, price: float) -> float:
         return (price - self.entry_price) / self.entry_price * 100 * self.size / abs(self.size)
 
     def get_pnl(self, price: float) -> float:
-        return price * abs(self.size) * self._get_roe(price) / 100
+        pnl = price * abs(self.size) * self._get_roe(price) / 100
+        return round(pnl, 2)
         # return price * self.size * (price - self.entry_price) / self.entry_price
 
-    def get_face_value(self, price: float) -> float:
+    def notional_value(self, price: float) -> float:
         return abs(price * self.size)
 
 
@@ -46,11 +54,14 @@ class Account:
         self.future_position = Position()
         self.usd_balance = init_balance
 
+        self.open_basis = 0
+
         self.perp_price = 0.
         self.future_price = 0.
         self.basis = 0.
-        self.trades_open = {}
-        self.trades_close = {}
+        self.date = None
+        self.trades_open = {}  # {date: basis}
+        self.trades_close = {}  # {date: basis}
 
     def __str__(self):
         return f'Final balance: {self.usd_balance}'
@@ -58,9 +69,10 @@ class Account:
     def _is_trade_on(self) -> bool:
         return self.perp_position.size != 0 or self.future_position.size != 0
 
-    def trade(self, date:str, perp_price: float, future_price: float):
+    def trade(self, date: str, perp_price: float, future_price: float):
         # todo flowchart
 
+        self.date = date
         self.perp_price = perp_price
         self.future_price = future_price
         self.basis = (perp_price - future_price) / perp_price * 100
@@ -68,29 +80,57 @@ class Account:
         # check if there is a trade to close
         if self._is_trade_on() and abs(self.basis) < MIN_THRESHOLD:
             self.close_trade()
-            self.trades_open[date] = self.basis
+            self.trades_close[date] = self.basis
         # check if there is a trade to open
         elif not self._is_trade_on() and abs(self.basis) >= MAX_THRESHOLD:
             self.open_trade()
+            self.trades_open[date] = self.basis
+            self.open_basis = self.basis
+
+        elif self._is_trade_on() and abs(self.basis - self.open_basis) > 1:
+            self.close_trade()
             self.trades_close[date] = self.basis
+            self.open_basis = 0
 
     def open_trade(self):
-        pass
-        # if self.basis > 0:
-        #     # sell perp, buy futures
-        #     print('open trade, sell {} perp, buy {} future')
-        # else:
-        #     # buy perp, sell futures
-        #     print('open trade, buy {} perp, sell {} future')
+        perp_amount = TRADE_AMOUNT / self.perp_price
+        future_amount = perp_amount
+        # self.usd_balance -= TRADE_AMOUNT + future_amount * self.future_price
+
+        if self.basis > 0:
+            # sell perp, buy futures
+            self.perp_position.update(self.perp_price, -perp_amount)
+            self.future_position.update(self.future_price, future_amount)
+            print(
+                f'{self.date} open trade, sell {round(perp_amount, 2)} perp @ {self.perp_price}, buy {round(future_amount, 2)} future @ {self.future_price}. Balance {self.usd_balance}')
+        else:
+            # buy perp, sell futures
+            self.perp_position.update(self.perp_price, perp_amount)
+            self.future_position.update(self.future_price, -future_amount)
+            print(
+                f'{self.date} open trade, buy {round(perp_amount, 2)} perp @ {self.perp_price}, sell {round(future_amount, 2)} future @ {self.future_price}. Balance {self.usd_balance}')
 
     def close_trade(self):
-        pass
-        # if True:
-        #     # sell perp, buy futures
-        #     print('open trade, sell {} perp, buy {} future')
-        # else:
-        #     # buy perp, sell futures
-        #     print('open trade, buy {} perp, sell {} future')
+        if self.perp_position.size > 0:
+            print(
+                f'{self.date} close trade, sell {round(self.perp_position.size, 2)} perp @ {self.perp_price} pnl {self.perp_position.get_pnl(self.perp_price)},'
+                f' buy {round(self.future_position.size, 2)} future @ {self.future_price} pnl {self.future_position.get_pnl(self.future_price)}. ',
+                end=' ')
+        else:
+            print(
+                f'{self.date} close trade, buy {round(self.perp_position.size, 2)} perp @ {self.perp_price} pnl {self.perp_position.get_pnl(self.perp_price)},'
+                f' sell {round(self.future_position.size, 2)} future @ {self.future_price} pnl {self.future_position.get_pnl(self.future_price)}. ',
+                end='')
+        profit = self.perp_position.get_pnl(self.perp_price) + self.future_position.get_pnl(self.future_price)
+        self.usd_balance += profit
+        self.perp_position.reset()
+        self.future_position.reset()
+        print(f'Profit: {round(profit, 2)}. Balance: {round(self.usd_balance, 2)}')
+
+        self.perp_position.entry_price = None
+        self.perp_position.size = 0
+        self.future_position.entry_price = None
+        self.future_position.size = 0
 
     def get_equity(self, perp_price: float, future_price: float) -> float:
         return self.usd_balance + self.perp_position.get_pnl(perp_price) + self.future_position.get_pnl(future_price)
@@ -278,35 +318,44 @@ class CarryBacktesting:
             future_price = self.future_prices[i]
             self.account.trade(date, perp_price, future_price)
 
-        self.account.close_trade()
+        # self.account.close_trade()
         print(self.account)
 
     def _plot(self):
         print('plotting')
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 6), sharex='col')
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 6), sharex='col')
         fig.suptitle(COIN)
 
+        # ax1
         ax1.plot(self.dates, self.perp_prices, linewidth=1)
         ax1.plot(self.dates, self.future_prices, linewidth=1)
         ax1.legend(['perp', 'future'])
         ax1.set_ylabel('$')
         ax1.grid()
 
-        basis = (self.perp_prices - self.future_prices) / self.perp_prices * 100
-        ax2.plot(self.dates, basis)
+        # ax2
+        basis = (self.perp_prices - self.future_prices)
+        ax2.plot(self.dates, basis, linewidth=1)
+        ax2.grid()
+        ax2.set_ylabel('$')
+        ax2.legend(['basis'])
+
+        # ax3
+        basis_perc = basis / self.perp_prices * 100
+        ax3.plot(self.dates, basis_perc, linewidth=1)
 
         # plot trades
         dates = self.account.trades_open.keys()
         trades_open = self.account.trades_open.values()
-        ax2.plot(dates, trades_open, 'o')
+        ax3.plot(dates, trades_open, 'ro', mfc='none')
 
         dates = self.account.trades_close.keys()
         trades_close = self.account.trades_close.values()
-        ax2.plot(dates, trades_close, 'x')
+        ax3.plot(dates, trades_close, 'rx')
 
-        ax2.legend(['basis', 'open', 'close'])
-        ax2.set_ylabel('%')
-        ax2.grid()
+        ax3.legend(['basis perc', 'open', 'close'])
+        ax3.set_ylabel('%')
+        ax3.grid()
 
         fig.autofmt_xdate()
         plt.show()
