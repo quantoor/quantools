caaf5ccd573690b481e17e0be49b51393a858e9a
Emanuele Bolognesi <35916369+adrift942@users.noreply.github.com>
2022-07-06 23:10:54 +0300

    refactoring

--- carry_backtesting.py
+++ carry_backtesting.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 import util
 from FtxClientRest import FtxClient
 import matplotlib.pyplot as plt
@@ -169,156 +168,11 @@ class CarryBacktesting:
         self.future_prices = np.array([])
         self.account = Account(0)
 
-    def carry(self):
-        futures = self.client.get_all_futures()
-
-        for i in futures:
-            if i['type'] == 'future' and not i['perpetual']:
-
-                spotSymbol = i['underlying']
-                futureSymbol = i['name']
-
-                try:
-                    ul_market = self.client.get_single_market(f'{spotSymbol}/USD')
-                except Exception as e:
-                    print(e)
-                    continue
-
-                ul_price = ul_market['price']
-                future_price = i['mark']
-
-                # higher = max(ul_price, futurePrice)
-                # lower = min(ul_price, futurePrice)
-                basis = (future_price - ul_price) / ul_price * 100
-
-                if abs(basis) > 3:
-                    print(f'{spotSymbol}: spot {ul_price}, {futureSymbol} {future_price}, basis {round(basis, 1)} %')
-
-    def analyze_carry(self):
-        # mean rev: YFI SUSHI FTM AAVE
-        # crazy: WAVES GST GMT BAL AXS
-
-        coins = {}  # {'coin': [a,b,c]}
-
-        markets = self.client.get_markets()
-        # expired_futures = client.get_expired_futures()
-        # expired_futures_btc = [i for i in expired_futures if i['underlying'] == 'BTC' and i['type'] == 'future']
-
-        for instrument in markets:
-
-            if instrument['quoteCurrency'] not in ['USD', None]:
-                continue
-            if instrument['futureType'] == 'move':
-                continue
-            if instrument['restricted']:
-                continue
-
-            ul_name = instrument['underlying']
-            if ul_name is None:
-                ul_name = instrument['baseCurrency']
-
-            if ul_name in ['BTC', 'CEL', 'DOGE', 'ETH', 'XRP', 'USDT']:
-                continue
-
-            if ul_name in coins:
-                coins[ul_name].append(instrument)
-            else:
-                coins[ul_name] = [instrument]
-
-        coins = {key: value for (key, value) in coins.items() if len(value) > 2}
-
-        start_timestamp = util.date_to_timestamp_sec(2022, 6, 1, 0)
-        end_timestamp = util.timestamp_now()
-
-        for coin in coins:
-            print(coin)
-            # spot_prices = client.get_historical_prices(f'{coin}/USD', 3600, start_timestamp, end_timestamp)
-            perp_prices = self.client.get_historical_prices(f'{coin}-PERP', 3600, start_timestamp, end_timestamp)
-            future_prices = self.client.get_historical_prices(f'{coin}-0930', 3600, start_timestamp, end_timestamp)
-
-            N = len(future_prices)
-            perp_prices = perp_prices[len(perp_prices) - N:]
-
-            times = [price['startTime'] for price in perp_prices]
-            dates = mdates.num2date(mdates.datestr2num(times))
-            dates = np.array(dates)
-
-            # spot_prices = np.array([price['close'] for price in spot_prices])
-            perp_prices = np.array([price['close'] for price in perp_prices])
-            future_prices = np.array([price['close'] for price in future_prices])
-
-            if len(perp_prices) != len(future_prices):
-                print('skip')
-                continue
-
-            fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 6), sharex='col')
-            fig.suptitle(coin)
-
-            # ax.plot(dates, spot_prices)
-            ax1.plot(dates, perp_prices)
-            ax1.plot(dates, future_prices)
-            ax1.legend(['perp', 'future'])
-            ax1.set_ylabel('$')
-            ax1.grid()
-
-            ax2.plot(dates, (perp_prices - future_prices) / perp_prices * 100)
-            ax2.set_ylabel('%')
-            ax2.grid()
-
-            fig.autofmt_xdate()
-
-        plt.show()
-
-    def plot_carry_expired(self):
-        print('start')
-
-        coin = 'AAVE'
-        resolution = 14400
-
-        start_timestamp = util.date_to_timestamp_sec(2022, 3, 20, 0)
-        end_timestamp = util.date_to_timestamp_sec(2022, 6, 24, 0)
-
-        print('getting perp prices')
-        perp_prices = self.client.get_historical_prices(f'{coin}-PERP', resolution, start_timestamp, end_timestamp)
-        print('getting future prices')
-        future_prices = self.client.get_historical_prices(f'{coin}-0624', resolution, start_timestamp, end_timestamp)
-
-        times = [price['startTime'] for price in perp_prices]
-        print(len(perp_prices), len(future_prices))
-
-        dates = mdates.num2date(mdates.datestr2num(times))
-        dates = np.array(dates)
-
-        perp_prices = np.array([price['close'] for price in perp_prices])
-        future_prices = np.array([price['close'] for price in future_prices])
-
-        print('plotting')
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 6), sharex='col')
-        fig.suptitle(coin)
-
-        ax1.plot(dates, perp_prices, linewidth=1)
-        ax1.plot(dates, future_prices, linewidth=1)
-        ax1.legend(['perp', 'future'])
-        ax1.set_ylabel('$')
-        ax1.grid()
-
-        ax2.plot(dates, (perp_prices - future_prices) / perp_prices * 100)
-        ax2.set_ylabel('%')
-        ax2.grid()
-
-        fig.autofmt_xdate()
-        plt.show()
-
-    def backtest_carry_expired(self):
-        print('backtest_carry_expired')
-        start_timestamp = util.date_to_timestamp_sec(2022, 3, 20, 0)
-        end_timestamp = util.date_to_timestamp_sec(2022, 6, 24, 0)
-
+    def backtest_carry(self, perp: str, future: str, start_ts: int, end_ts: int):
         print('getting historical prices')
         resolution = 14400
-        perp_prices = self.client.get_historical_prices(f'{COIN}-PERP', resolution, start_timestamp, end_timestamp)
-        future_prices = self.client.get_historical_prices(f'{COIN}-0624', resolution, start_timestamp,
-                                                          end_timestamp)
+        perp_prices = self.client.get_historical_prices(perp, resolution, start_ts, end_ts)
+        future_prices = self.client.get_historical_prices(future, resolution, start_ts, end_ts)
 
         assert (len(perp_prices) == len(future_prices))
 
@@ -385,6 +239,149 @@ class CarryBacktesting:
 
 
 if __name__ == '__main__':
+    _start_ts = util.date_to_timestamp_sec(2022, 3, 20, 0)
+    _end_ts = util.date_to_timestamp_sec(2022, 6, 24, 0)
+
     backtester = CarryBacktesting()
-    backtester.backtest_carry_expired()
+    backtester.backtest_carry(f'{COIN}-PERP', f'{COIN}-0624', _start_ts, _end_ts)
     print('done')
+
+    # def carry(self):
+    #     futures = self.client.get_all_futures()
+    #
+    #     for i in futures:
+    #         if i['type'] == 'future' and not i['perpetual']:
+    #
+    #             spotSymbol = i['underlying']
+    #             futureSymbol = i['name']
+    #
+    #             try:
+    #                 ul_market = self.client.get_single_market(f'{spotSymbol}/USD')
+    #             except Exception as e:
+    #                 print(e)
+    #                 continue
+    #
+    #             ul_price = ul_market['price']
+    #             future_price = i['mark']
+    #
+    #             # higher = max(ul_price, futurePrice)
+    #             # lower = min(ul_price, futurePrice)
+    #             basis = (future_price - ul_price) / ul_price * 100
+    #
+    #             if abs(basis) > 3:
+    #                 print(f'{spotSymbol}: spot {ul_price}, {futureSymbol} {future_price}, basis {round(basis, 1)} %')
+    #
+    # def analyze_carry(self):
+    #     # mean rev: YFI SUSHI FTM AAVE
+    #     # crazy: WAVES GST GMT BAL AXS
+    #
+    #     coins = {}  # {'coin': [a,b,c]}
+    #
+    #     markets = self.client.get_markets()
+    #     # expired_futures = client.get_expired_futures()
+    #     # expired_futures_btc = [i for i in expired_futures if i['underlying'] == 'BTC' and i['type'] == 'future']
+    #
+    #     for instrument in markets:
+    #
+    #         if instrument['quoteCurrency'] not in ['USD', None]:
+    #             continue
+    #         if instrument['futureType'] == 'move':
+    #             continue
+    #         if instrument['restricted']:
+    #             continue
+    #
+    #         ul_name = instrument['underlying']
+    #         if ul_name is None:
+    #             ul_name = instrument['baseCurrency']
+    #
+    #         if ul_name in ['BTC', 'CEL', 'DOGE', 'ETH', 'XRP', 'USDT']:
+    #             continue
+    #
+    #         if ul_name in coins:
+    #             coins[ul_name].append(instrument)
+    #         else:
+    #             coins[ul_name] = [instrument]
+    #
+    #     coins = {key: value for (key, value) in coins.items() if len(value) > 2}
+    #
+    #     start_timestamp = util.date_to_timestamp_sec(2022, 6, 1, 0)
+    #     end_timestamp = util.timestamp_now()
+    #
+    #     for coin in coins:
+    #         print(coin)
+    #         # spot_prices = client.get_historical_prices(f'{coin}/USD', 3600, start_timestamp, end_timestamp)
+    #         perp_prices = self.client.get_historical_prices(f'{coin}-PERP', 3600, start_timestamp, end_timestamp)
+    #         future_prices = self.client.get_historical_prices(f'{coin}-0930', 3600, start_timestamp, end_timestamp)
+    #
+    #         N = len(future_prices)
+    #         perp_prices = perp_prices[len(perp_prices) - N:]
+    #
+    #         times = [price['startTime'] for price in perp_prices]
+    #         dates = mdates.num2date(mdates.datestr2num(times))
+    #         dates = np.array(dates)
+    #
+    #         # spot_prices = np.array([price['close'] for price in spot_prices])
+    #         perp_prices = np.array([price['close'] for price in perp_prices])
+    #         future_prices = np.array([price['close'] for price in future_prices])
+    #
+    #         if len(perp_prices) != len(future_prices):
+    #             print('skip')
+    #             continue
+    #
+    #         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 6), sharex='col')
+    #         fig.suptitle(coin)
+    #
+    #         # ax.plot(dates, spot_prices)
+    #         ax1.plot(dates, perp_prices)
+    #         ax1.plot(dates, future_prices)
+    #         ax1.legend(['perp', 'future'])
+    #         ax1.set_ylabel('$')
+    #         ax1.grid()
+    #
+    #         ax2.plot(dates, (perp_prices - future_prices) / perp_prices * 100)
+    #         ax2.set_ylabel('%')
+    #         ax2.grid()
+    #
+    #         fig.autofmt_xdate()
+    #
+    #     plt.show()
+    #
+    # def plot_carry_expired(self):
+    #     print('start')
+    #
+    #     coin = 'AAVE'
+    #     resolution = 14400
+    #
+    #     start_timestamp = util.date_to_timestamp_sec(2022, 3, 20, 0)
+    #     end_timestamp = util.date_to_timestamp_sec(2022, 6, 24, 0)
+    #
+    #     print('getting perp prices')
+    #     perp_prices = self.client.get_historical_prices(f'{coin}-PERP', resolution, start_timestamp, end_timestamp)
+    #     print('getting future prices')
+    #     future_prices = self.client.get_historical_prices(f'{coin}-0624', resolution, start_timestamp, end_timestamp)
+    #
+    #     times = [price['startTime'] for price in perp_prices]
+    #     print(len(perp_prices), len(future_prices))
+    #
+    #     dates = mdates.num2date(mdates.datestr2num(times))
+    #     dates = np.array(dates)
+    #
+    #     perp_prices = np.array([price['close'] for price in perp_prices])
+    #     future_prices = np.array([price['close'] for price in future_prices])
+    #
+    #     print('plotting')
+    #     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 6), sharex='col')
+    #     fig.suptitle(coin)
+    #
+    #     ax1.plot(dates, perp_prices, linewidth=1)
+    #     ax1.plot(dates, future_prices, linewidth=1)
+    #     ax1.legend(['perp', 'future'])
+    #     ax1.set_ylabel('$')
+    #     ax1.grid()
+    #
+    #     ax2.plot(dates, (perp_prices - future_prices) / perp_prices * 100)
+    #     ax2.set_ylabel('%')
+    #     ax2.grid()
+    #
+    #     fig.autofmt_xdate()
+    #     plt.show()
