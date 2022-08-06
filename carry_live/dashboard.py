85a679fe5b8452ff431ef175140c525b6b754016
Emanuele Bolognesi <35916369+adrift942@users.noreply.github.com>
2022-08-06 20:09:16 +0300

    refactor live dashboard

--- carry_live/dashboard.py
+++ carry_live/dashboard.py
@@ -18,29 +18,81 @@ ACTIVE_FUTURES = util.get_active_futures_with_expiry()
 EXPIRY = '0930'
 COINS = [coin for coin in ACTIVE_FUTURES[EXPIRY] if coin not in config.BLACKLIST]
 
-if st.button('Cancel open orders'):
-    connector_rest = FtxConnectorRest(config.API_KEY, config.API_SECRET, config.SUB_ACCOUNT)
-    connector_rest.cancel_orders()
 
-st.header('Active coins:')
-st.subheader(COINS)
+# st.header('Active coins:')
+# st.subheader(COINS)
+MODES = ('Market Overview', 'Positions', 'Open Orders')
 
-data_table = st.empty()
 
-while True:
-    data = []
+def main():
+    with st.sidebar:
+        mode = st.radio("Mode", MODES)
 
-    for coin in COINS:
-        cache_path = f'{config.CACHE_FOLDER}/{coin}.json'
-        if not util.file_exists(cache_path):
-            continue
+    if mode == MODES[0]:
+        show_market_overview()
+    elif mode == MODES[1]:
+        show_positions()
+    elif mode == MODES[2]:
+        show_open_orders()
 
-        cache = Cache(cache_path)
-        data.append(cache.get_dict())
 
-    df = pd.DataFrame(data)
-    df['basis'] = (df['perp_price'] - df['fut_price']) / df['perp_price'] * 100
-    df.columns = ['Coin', 'LOB', 'COT', 'Perp Price', 'Perp Size', 'Fut Price', 'Fut Size', 'Basis']
-    data_table.table(df)
+def show_market_overview():
+    st.title(MODES[0])
 
-    time.sleep(config.REFRESH_TIME)
+    data_table = st.empty()
+
+    while True:
+        data = []
+
+        for coin in COINS:
+            cache_path = f'{config.CACHE_FOLDER}/{coin}.json'
+            if not util.file_exists(cache_path):
+                continue
+
+            cache = Cache(cache_path)
+            data.append(cache.get_dict())
+
+        df = pd.DataFrame(data)
+        df['basis'] = (df['perp_price'] - df['fut_price']) / df['perp_price'] * 100
+        df.columns = ['Coin', 'LOB', 'COT', 'Perp Price', 'Perp Size', 'Fut Price', 'Fut Size', 'Basis']
+        df.drop(['LOB', 'COT', 'Perp Size', 'Fut Size'], axis=1, inplace=True)
+        data_table.table(df)
+
+        time.sleep(1)
+
+
+def show_positions():
+    st.title(MODES[1])
+
+    data_table = st.empty()
+
+    while True:
+        data = []
+
+        for coin in COINS:
+            cache_path = f'{config.CACHE_FOLDER}/{coin}.json'
+            if not util.file_exists(cache_path):
+                continue
+
+            cache = Cache(cache_path)
+            if cache.perp_size or cache.fut_size:
+                data.append(cache.get_dict())
+
+        df = pd.DataFrame(data)
+        df['basis'] = (df['perp_price'] - df['fut_price']) / df['perp_price'] * 100
+        df.columns = ['Coin', 'LOB', 'COT', 'Perp Price', 'Perp Size', 'Fut Price', 'Fut Size', 'Basis']
+        df.drop(['Perp Price', 'Fut Price'], axis=1, inplace=True)
+        data_table.table(df)
+
+        time.sleep(1)
+
+
+def show_open_orders():
+    st.title(MODES[2])
+
+    if st.button('Cancel open orders'):
+        connector_rest = FtxConnectorRest(config.API_KEY, config.API_SECRET, config.SUB_ACCOUNT)
+        connector_rest.cancel_orders()
+
+
+main()
