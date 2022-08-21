9a1d49b5241be2f6c8d7bab27a4ad85667ff98c3
Emanuele Bolognesi <emanuele.bolognesi@nexo.io>
2022-08-21 21:32:15 +0300

    added carry live bot

---
+++ carry_live/config.py
@@ -0,0 +1,42 @@
+from typing import List
+import json
+
+CACHE_FOLDER: str
+LOG_FOLDER: str
+API_KEY: str
+API_SECRET: str
+SUB_ACCOUNT: str
+TELEGRAM_TOKEN: str
+TELEGRAM_CHAT_ID: str
+BLACKLIST: List[str]
+WHITELIST: List[str]
+REFRESH_TIME: float
+LIVE_TRADE: bool
+TRADE_SIZE_USD: float
+INIT_OPEN_THRESHOLD: float
+THRESHOLD_INCREMENT: float
+
+with open('./config.json') as f:
+    data = json.load(f)
+
+    CACHE_FOLDER = data['cache_folder']
+    LOG_FOLDER = data['log_folder']
+
+    ftx = data['ftx']
+    API_KEY = ftx['api_key']
+    API_SECRET = ftx['api_secret']
+    SUB_ACCOUNT = ftx['sub_account']
+
+    telegram = data['telegram']
+    TELEGRAM_TOKEN = telegram['token']
+    TELEGRAM_CHAT_ID = telegram['chat_id']
+
+    BLACKLIST = sorted(data['blacklist'])
+    WHITELIST = sorted(data['whitelist'])
+    REFRESH_TIME = data['refresh_time']
+    LIVE_TRADE = data['live_trade']
+
+    strategy = data['strategy']
+    TRADE_SIZE_USD = strategy['trade_size_usd']
+    INIT_OPEN_THRESHOLD = strategy['init_open_threshold']
+    THRESHOLD_INCREMENT = strategy['threshold_increment']
---
+++ carry_live/dashboard.py
@@ -0,0 +1,143 @@
+import importer
+import streamlit as st
+from quantools.common import util
+import config
+from ftx_connector import FtxConnectorRest
+from quantools.common.FtxClientWs import FtxWebsocketClient
+from types_ import Cache, WsTicker
+import time
+import pandas as pd
+import numpy as np
+import asyncio
+import json
+
+ws_client = FtxWebsocketClient()
+connector_rest = FtxConnectorRest(config.API_KEY, config.API_SECRET, config.SUB_ACCOUNT)
+
+EXPIRY = '0930'
+
+MODES = ('Market Overview', 'Positions', 'Open Orders')
+
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+
+def main():
+    with st.sidebar:
+        mode = st.radio("Mode", MODES)
+
+    if mode == MODES[0]:
+        show_market_overview()
+    elif mode == MODES[1]:
+        show_positions()
+    elif mode == MODES[2]:
+        show_open_orders()
+
+
+_active_futures = util.get_active_futures_with_expiry()
+_expiry = '0930'
+_coins = [coin for coin in _active_futures[_expiry] if coin not in config.BLACKLIST]
+fundings = {coin: None for coin in _coins}
+
+
+def show_market_overview():
+    st.title(MODES[0])
+    loop = asyncio.new_event_loop()
+    # loop.create_task(_get_funding())
+    loop.create_task(_market_overview())
+    # loop.run_forever()
+
+
+async def _get_funding():
+    while True:
+        for coin in _coins:
+            fundings[coin] = util.get_funding_rate_avg_24h(util.get_perp_symbol(coin))
+            _ = await asyncio.sleep(0.01)
+        _ = await asyncio.sleep(30)
+
+
+async def _market_overview():
+    data_table = st.empty()
+    while True:
+        market_data = []  # ['Coin', 'Perp Price', 'Fut Price', 'Basis', 'Funding']
+
+        def _get_ticker(market: str):
+            res = ws_client.get_ticker(market)
+            if len(res) == 0:
+                return None
+            return WsTicker(res)
+
+        for coin in _coins:
+            perp_symbol = util.get_perp_symbol(coin)
+            fut_symbol = util.get_future_symbol(coin, _expiry)
+
+            perp_ticker = _get_ticker(perp_symbol)
+            fut_ticker = _get_ticker(fut_symbol)
+
+            if perp_ticker is None or fut_ticker is None:
+                continue
+
+            perp_price = perp_ticker.mark
+            fut_price = fut_ticker.mark
+
+            basis = util.get_basis(perp_price, fut_price)
+
+            if abs(basis) > 1:
+                market_data.append({
+                    'Coin': coin,
+                    'Perp Price': perp_price,
+                    'Fut Price': fut_price,
+                    'Basis': basis,
+                    'Funding': None,  # util.get_funding_rate_avg_24h(perp_symbol)
+                })
+
+        df = pd.DataFrame(market_data)
+        data_table.table(df)
+
+        _ = await asyncio.sleep(1)
+
+
+def show_positions():
+    st.title(MODES[1])
+
+    data_table = st.empty()
+
+    while True:
+        data = []
+
+        for coin in config.WHITELIST:
+            cache_path = f'{config.CACHE_FOLDER}/{coin}.json'
+            if not util.file_exists(cache_path):
+                continue
+
+            cache = Cache()
+            cache.read(cache_path)
+            if cache.perp_size or cache.fut_size:
+                data.append(cache.get_dict())
+
+        df = pd.DataFrame(data)
+        df.columns = ['Coin', 'LOB', 'COT', 'Perp Size', 'Fut Size', 'Basis', 'Adj Basis Open', 'Adj Basis Close',
+                      'Funding']
+        # df.drop(['Perp Price', 'Fut Price'], axis=1, inplace=True)
+        # df = df.reindex(['Coin', 'Perp Size', 'Fut Size', 'Basis', 'Funding'], axis=1)
+        data_table.table(df)
+
+        time.sleep(1)
+
+
+def show_open_orders():
+    st.title(MODES[2])
+
+    if st.button('Cancel open orders'):
+        connector_rest.cancel_orders()
+
+
+main()
