a7889cc54cba3a627b147d9f13d0a6f7ddf15656
Emanuele Bolognesi <35916369+adrift942@users.noreply.github.com>
2022-09-11 11:31:02 +0300

    reconciliate trades from trades raw

--- carry_live/db_client.py
+++ carry_live/db_client.py
@@ -3,19 +3,18 @@ from classes import Trade, Position
 
 
 class DbClient:
-    TRADES_TABLE = 'trades'
-    ORDERS_TABLE = 'orders'
+    TRADES_RAW_TABLE = 'trades_raw'
     POSITIONS_TABLE = 'positions'
+    TRADES_TABLE = 'trades'
 
     def __init__(self, db_full_path: str):
         self._conn = sqlite3.connect(db_full_path)
         self._create_tables()
 
     def _create_tables(self):
-        query = f'''CREATE TABLE IF NOT EXISTS {self.TRADES_TABLE} (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+        query = f'''CREATE TABLE IF NOT EXISTS {self.TRADES_RAW_TABLE} (
                 instrument TEXT NOT NULL,
-                trade_id INTEGER NOT NULL UNIQUE,
+                trade_id INTEGER PRIMARY KEY,
                 order_id INTEGER NOT NULL,
                 side TEXT NOT NULL,
                 price REAL NOT NULL,                
@@ -35,16 +34,27 @@ class DbClient:
                 '''
         self._conn.execute(query)
 
-    def insert_trade(self, t: Trade) -> bool:
-        query = f'''INSERT INTO {self.TRADES_TABLE} (instrument, trade_id, order_id, side, price, amount, fee, maker, timestamp, date)
+        query = f'''CREATE TABLE IF NOT EXISTS {self.TRADES_TABLE} (
+                instrument TEXT NOT NULL,
+                order_id INTEGER PRIMARY KEY,
+                n_trades INTEGER NOT NULL,
+                side TEXT NOT NULL,
+                price REAL NOT NULL,                
+                amount REAL NOT NULL,     
+                fee REAL NOT NULL,       
+                timestamp INTEGER NOT NULL);
+                '''
+        self._conn.execute(query)
+
+    def insert_trade_raw(self, t: Trade) -> None:
+        query = f'''INSERT INTO {self.TRADES_RAW_TABLE} (instrument, trade_id, order_id, side, price, amount, fee, maker, timestamp, date)
                 VALUES ('{t.instrument}', {t.trade_id}, {t.order_id}, '{t.side}', {t.price}, {t.amount}, {t.fee}, {t.maker}, {t.timestamp}, '{t.date}');
                 '''
         try:
             self._conn.execute(query)
             self._conn.commit()
-            return True
         except sqlite3.IntegrityError:
-            return False
+            pass
 
     def insert_position(self, p: Position) -> bool:
         side = 'long' if p.is_long else 'short'
@@ -58,8 +68,40 @@ class DbClient:
         except sqlite3.IntegrityError:
             return False
 
-    def get_trade(self):
-        query = f'''SELECT * FROM {self.TRADES_TABLE};'''
+    def insert_trade(self, instrument, order_id, n_trades, side, price, amount, fee, timestamp) -> None:
+        query = f'''INSERT INTO {self.TRADES_TABLE} (instrument, order_id, n_trades, side, price, amount, fee, timestamp)
+                    VALUES ('{instrument}', {order_id}, {n_trades}, '{side}', {price}, {amount}, {fee}, {timestamp});
+                    '''
+        try:
+            self._conn.execute(query)
+            self._conn.commit()
+        except sqlite3.IntegrityError:
+            pass
+
+    def insert_trades(self):
+        query = f'''SELECT 
+                        instrument,
+                        order_id,
+                        COUNT(*) as n_trades,
+                        side,
+                        SUM(price * amount) / SUM(amount) as price,
+                        SUM(amount) as amount,
+                        SUM(fee) as fee,
+                        MIN(timestamp) as timestmap
+                    FROM
+                        {self.TRADES_RAW_TABLE}
+                    GROUP BY
+                        order_id;
+                    '''
         cur = self._conn.cursor()
         cur.execute(query)
-        return cur.fetchall()
+        trades = cur.fetchall()
+
+        for trade in trades:
+            self.insert_trade(*trade)
+
+    # def get_trade(self):
+    #     query = f'''SELECT * FROM {self.TRADES_TABLE};'''
+    #     cur = self._conn.cursor()
+    #     cur.execute(query)
+    #     return cur.fetchall()
--- carry_live/reconciliation.py
+++ carry_live/reconciliation.py
@@ -7,7 +7,7 @@ ftx_client = FtxConnectorRest(cfg.API_KEY, cfg.API_SECRET, cfg.SUB_ACCOUNT)
 db_client = DbClient('./db/30SEP22.db')
 
 
-def trades():
+def dump_trades_raw():
     active_futures = util.get_active_futures_with_expiry()
     expiration = '0930'
     coins = [coin for coin in active_futures[expiration]]
@@ -16,18 +16,24 @@ def trades():
         print(coin)
 
         for trade in ftx_client.get_trades(util.get_perp_symbol(coin)):
-            db_client.insert_trade(trade)
+            db_client.insert_trade_raw(trade)
 
         for trade in ftx_client.get_trades(util.get_future_symbol(coin, expiration)):
-            db_client.insert_trade(trade)
+            db_client.insert_trade_raw(trade)
 
 
-def positions():
+def dump_positions():
     for position in ftx_client.get_positions():
         print(position.symbol)
         db_client.insert_position(position)
 
 
-trades()
-positions()
+def reconciliate_trades():
+    db_client.insert_trades()
+
+
+# dump_trades_raw()
+# dump_positions()
+reconciliate_trades()
+
 print('done')
