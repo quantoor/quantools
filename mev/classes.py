9c82ecf5f7cc2afeddf80a0197746f93cddf3bf4
Emanuele Bolognesi <emanuele.bolognesi@nexo.io>
2022-10-07 16:59:16 +0300

    added LP class and get_tokens_out_from_tokens_in

--- mev/classes.py
+++ mev/classes.py
@@ -7,3 +7,52 @@ class Token:
         self.address = contract.address
         self.symbol = contract.symbol()
         self.decimals = contract.decimals()
+
+
+class LP:
+    def __init__(self, address, alias):
+        self._lp = util.load_contract(address, alias)
+        self._alias = alias
+        self._token0 = None
+        self._token1 = None
+
+    def get_reserves(self):
+        return self._lp.getReserves()
+
+    def get_tokens_out_from_tokens_in(self, quantity_token_in, is_token0_in, fee='0.003'):
+        reserve_token0, reserve_token1, _ = self.get_reserves()
+
+        if is_token0_in:
+            quantity_token_in *= 10 ** self.token0.decimals
+            amount_out = util.get_tokens_out_from_tokens_in(reserve_token0,
+                                                            reserve_token1,
+                                                            quantity_token0_in=quantity_token_in,
+                                                            fee=fee)
+            return amount_out / 10 ** self.token1.decimals
+        else:
+            quantity_token_in *= 10 ** self.token1.decimals
+            amount_out = util.get_tokens_out_from_tokens_in(reserve_token0,
+                                                            reserve_token1,
+                                                            quantity_token1_in=quantity_token_in,
+                                                            fee=fee)
+            return amount_out / 10 ** self.token0.decimals
+
+    @property
+    def token0(self):
+        if self._token0 is None:
+            self._token0 = Token(self._lp.token0(), self._alias + 'token0')
+        return self._token0
+
+    @token0.setter
+    def token0(self, value):
+        self._token0 = value
+
+    @property
+    def token1(self):
+        if self._token1 is None:
+            self._token1 = Token(self._lp.token1(), self._alias + 'token1')
+        return self._token1
+
+    @token1.setter
+    def token1(self, value):
+        self._token1 = value
--- mev/util.py
+++ mev/util.py
@@ -1,5 +1,6 @@
 from brownie import Contract
 from common.logger import logger
+from decimal import Decimal
 
 
 def load_contract(address, alias):
@@ -12,3 +13,25 @@ def load_contract(address, alias):
         logger.error(f'Could not load contract {address}: {e}')
         return None
     return contract
+
+
+def get_tokens_out_from_tokens_in(
+        reserve_token0,
+        reserve_token1,
+        quantity_token0_in=0,
+        quantity_token1_in=0,
+        fee='0.003'
+):
+    fee = Decimal(fee)
+
+    if quantity_token0_in and quantity_token1_in:
+        raise Exception('Both token quantity in provided')
+
+    if not quantity_token0_in and not quantity_token1_in:
+        raise Exception('Quantity token in not provided')
+
+    if quantity_token0_in:
+        return (reserve_token1 * quantity_token0_in * (1 - fee)) // (reserve_token0 + quantity_token0_in * (1 - fee))
+
+    if quantity_token1_in:
+        return (reserve_token0 * quantity_token1_in * (1 - fee)) // (reserve_token1 + quantity_token1_in * (1 - fee))
