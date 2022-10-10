e2317cc9091ca81894f6c88e00bf154355cfc563
Emanuele Bolognesi <emanuele.bolognesi@nexo.io>
2022-10-10 12:28:11 +0300

    store pool info in db

--- mev/demo_multicall.py
+++ mev/demo_multicall.py
@@ -3,6 +3,8 @@ import sys, os, csv
 import copy
 import warnings
 import time
+import util
+import sqlite3
 
 warnings.filterwarnings("ignore", category=exceptions.BrownieCompilerWarning)
 
@@ -22,38 +24,31 @@ def main():
     exchanges = [
         {
             "name": "SushiSwap",
-            "filename": "sushiswap_pools.csv",
+            "table_name": "sushiswap_pools",
             "factory_address": "0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
         },
         {
             "name": "TraderJoe",
-            "filename": "traderjoe_pools.csv",
+            "table_name": "traderjoe_pools",
             "factory_address": "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10"
         },
         {
             "name": "Pangolin",
-            "filename": "pangolin_pools.csv",
+            "table_name": "pangolin_pools",
             "factory_address": "0xefa94DE7a4656D787667C749f7E1223D71E9FD88"
         }
     ]
 
-    for name, factory_address, filename in [
-        (exchange["name"], exchange["factory_address"], exchange["filename"])
+    for name, factory_address, table_name in [
+        (exchange["name"], exchange["factory_address"], exchange["table_name"])
         for exchange in exchanges
     ]:
 
         print(f"Exchange {name}")
+        factory = util.load_contract(factory_address, name)
 
-        try:
-            factory = Contract.from_explorer(address=factory_address, silent=True)
-        except exceptions.BrownieCompilerWarning:
-            pass
-
-            # print(f"Retrieving ABI for typical LP")
-        try:
-            LP_ABI = Contract.from_explorer(address=factory.allPairs(0), silent=True).abi
-        except exceptions.BrownieCompilerWarning:
-            pass
+        # print(f"Retrieving ABI for typical LP")
+        LP_ABI = util.load_contract(factory.allPairs(0), name + 'abi').abi
 
         current_block = chain.height
 
@@ -65,67 +60,88 @@ def main():
         # from the factory (using multicall)
         pool_addresses = []
 
-        start = time.time()
         with multicall(block_identifier=current_block):
-            for pool_id in range(pool_count):
+            for i, pool_id in enumerate(range(pool_count)):
                 if pool_id % FLUSH_INTERVAL == 0 and pool_id != 0:
                     multicall.flush()
-                    # print(f"Found {i} pools")
+                    print(f"Found {i}/{pool_count} pools")
                 pool_addresses.append(factory.allPairs(pool_id))
-        print(f'Found {len(pool_addresses)} pools in {time.time() - start} s')
-        return
 
-        # print("• Creating LP objects")
-        # pools = []
-        # for i, address in enumerate(pool_addresses):
-        #     if i % FLUSH_INTERVAL == 0 and i != 0:
-        #         print(f"created {i} pool objects")
-        #     pools.append(Contract.from_abi(
-        #         name="",
-        #         address=address,
-        #         abi=LP_ABI
-        #     ))
-        #     print(f"created {i} objects")
-        #
-        # print("• Getting token0 data")
-        # pool_token0_addresses = []
-        # with multicall(block_identifier=current_block):
-        #     for i, pool_object in enumerate(pools):
-        #         if i % FLUSH_INTERVAL == 0 and i != 0:
-        #             multicall.flush()
-        #             print(f"fetched {i} addresses")
-        #         pool_token0_addresses.append(pool_object.token0())
-        #     print(f"fetched {len(pools)} addresses")
-        #
-        # print("• Getting token1 data")
-        # pool_token1_addresses = []
-        # with multicall(block_identifier=current_block):
-        #     for i, pool_object in enumerate(pools):
-        #         if i % FLUSH_INTERVAL == 0 and i != 0:
-        #             multicall.flush()
-        #             print(f"fetched {i} addresses")
-        #         pool_token1_addresses.append(pool_object.token1())
-        #     print(f"fetched {len(pools)} addresses")
-        #
-        # rows = list(
-        #     zip(
-        #         pool_addresses,
-        #         pool_token0_addresses,
-        #         pool_token1_addresses,
-        #     )
-        # )
-        #
-        # print("• Saving pool data to CSV")
-        # headers = [
-        #     "pool_address",
-        #     "token0",
-        #     "token1",
-        # ]
-        # with open(filename, "w") as file:
-        #     csv_writer = csv.writer(file)
-        #     csv_writer.writerow(headers)
-        #     csv_writer.writerows(rows)
-        # return
+        print("• Creating LP objects")
+        pools = []
+        for i, address in enumerate(pool_addresses):
+            if i % FLUSH_INTERVAL == 0 and i != 0:
+                multicall.flush()
+                print(f"created {i}/{len(pool_addresses)} pool objects")
+            pools.append(Contract.from_abi(
+                name="",
+                address=address,
+                abi=LP_ABI
+            ))
+
+        print("• Getting token0 data")
+        pool_token0_addresses = []
+        with multicall(block_identifier=current_block):
+            for i, pool_object in enumerate(pools):
+                if i % FLUSH_INTERVAL == 0 and i != 0:
+                    multicall.flush()
+                    print(f"fetched {i}/{len(pools)} addresses")
+                pool_token0_addresses.append(pool_object.token0())
+
+        print("• Getting token1 data")
+        pool_token1_addresses = []
+        with multicall(block_identifier=current_block):
+            for i, pool_object in enumerate(pools):
+                if i % FLUSH_INTERVAL == 0 and i != 0:
+                    multicall.flush()
+                    print(f"fetched {i}/{len(pools)} addresses")
+                pool_token1_addresses.append(pool_object.token1())
+
+        # list of (pool, token0, token1)
+        rows = list(
+            zip(
+                pool_addresses,
+                pool_token0_addresses,
+                pool_token1_addresses,
+            )
+        )
+
+        conn = sqlite3.connect('./pools.db')
+        create_table(conn, table_name)
+        for row in rows:
+            insert(conn, table_name, *row)
+
+
+def create_table(conn, table_name):
+    query = f'''CREATE TABLE IF NOT EXISTS {table_name} (
+            pool TEXT NOT NULL PRIMARY KEY,
+            token0 TEXT NOT NULL,
+            token1 TEXT NOT NULL);
+            '''
+    conn.execute(query)
+
+
+def insert(conn, table_name, pool, token0, token1):
+    query = f'''INSERT INTO {table_name} (pool, token0, token1)
+            VALUES ('{pool}', '{token0}', '{token1}');
+            '''
+    try:
+        conn.execute(query)
+        conn.commit()
+    except sqlite3.IntegrityError:
+        pass
+    except Exception as e:
+        print(f'Could not insert in {table_name} pool {pool}: {e}')
+
+
+def get(conn, table_name):
+    query = f'''SELECT * FROM {table_name};'''
+    cur = conn.cursor()
+    cur.execute(query)
+    return cur.fetchall()
 
 
 main()
+# conn = sqlite3.connect('./pools.db')
+# res = get(conn, 'sushiswap_pools')
+# print(res)
