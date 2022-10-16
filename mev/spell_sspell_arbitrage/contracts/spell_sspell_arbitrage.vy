1bb696b1ccdc34b252dbae50b1d1d7cfce3f6fe9
Emanuele Bolognesi <35916369+adrift942@users.noreply.github.com>
2022-10-16 17:59:37 +0300

    implementing spell-sspell arbitrage

---
+++ mev/spell_sspell_arbitrage/contracts/spell_sspell_arbitrage.vy
@@ -0,0 +1,138 @@
+# @version >=0.3.2
+
+from vyper.interfaces import ERC20
+
+interface IUniswapV2Pair:
+  def token0() -> address: view
+  def token1() -> address: view
+  def swap(
+    amount0Out: uint256,
+    amount1Out: uint256,
+    to: address,
+    data: Bytes[32],
+  ): nonpayable
+
+interface IUniswapV2Router:
+  def swapExactTokensForTokens(
+    amountOut: uint256,
+    amountInMax: uint256,
+    path: DynArray[address, 16],
+    to: address,
+    deadline: uint256
+  ) -> DynArray[uint256, 16]: nonpayable
+  def getAmountsIn(
+    amountOut: uint256,
+    path: DynArray[address, 16]
+  ) -> DynArray[uint256, 16]: view
+
+interface IUniswapV2Callee:
+  def uniswapV2Call(
+    sender: address,
+    amount0: uint256,
+    amount1: uint256,
+    data: Bytes[32],
+  ): nonpayable
+
+implements: IUniswapV2Callee
+
+deadline: constant(uint256) = 60  # 60 seconds expiration for router swap
+
+# these values are set by the constructor during contract creation
+sushiswap_router_address: address
+
+# these values are dynamic, controlled at execution time by arguments passed to the execute() function
+flash_borrow_pool_address: address
+swap_router: IUniswapV2Router
+swap_path: DynArray[address, 16]
+
+@external
+@nonpayable
+def __init__(_sushiswap_router_address: address):
+  self.sushiswap_router_address = _sushiswap_router_address
+
+@external
+@nonpayable
+def execute(
+  flash_borrow_pool_address: address,
+  flash_borrow_token_address: address,
+  flash_borrow_token_amount: uint256,
+  swap_path: DynArray[address, 16],
+  swap_router_address: address
+):
+
+  # build a path for the swap (may be up to 16 tokens long)
+  self.swap_path = swap_path
+
+  # create a pointer to the external router
+  self.swap_router = IUniswapV2Router(swap_router_address)
+
+  # store the flash borrowing pool address, needed to access the borrowing pool inside the callback
+  self.flash_borrow_pool_address = flash_borrow_pool_address
+
+  amount0: uint256 = 0
+  amount1: uint256 = 0
+
+  # sets "unlimited" approval whenever this contract attempts to interact with this token and the approval is less than the requested amount
+  approval: uint256 = ERC20(flash_borrow_token_address).allowance(self, swap_router_address)
+  if approval < flash_borrow_token_amount:
+    ERC20(flash_borrow_token_address).approve(swap_router_address, MAX_UINT256)
+
+  if flash_borrow_token_address == IUniswapV2Pair(flash_borrow_pool_address).token0():
+    amount0 = flash_borrow_token_amount
+  else:
+    amount1 = flash_borrow_token_amount
+
+  IUniswapV2Pair(flash_borrow_pool_address).swap(amount0, amount1, self, b'flash')
+
+@external
+@nonpayable
+def uniswapV2Call(
+  _sender: address,
+  _amount0: uint256,
+  _amount1: uint256,
+  _data: Bytes[32],
+  ):
+
+  # Use the Pair interface to retrieve and store the addresses for token0 and token1
+  token0: address = IUniswapV2Pair(msg.sender).token0()
+  token1: address = IUniswapV2Pair(msg.sender).token1()
+
+  # use the call amounts to determine which token was transferred from the LP, and to build the path array
+  # before calling for the router swap
+  amount_borrow: uint256 = 0
+  _path: DynArray[address, 2] = []
+
+  if _amount0 > 0:
+    amount_borrow = _amount0
+    _path.append(token1)
+    _path.append(token0)
+  else:
+    amount_borrow = _amount1
+    _path.append(token0)
+    _path.append(token1)
+
+  # calculate amount_repay (INPUT) needed to withdraw amount_borrow (OUTPUT),
+  # used to repay the flash borrow later
+  amount_repay: uint256 = IUniswapV2Router(self.sushiswap_router_address).getAmountsIn(
+    amount_borrow,
+    _path
+  )[0]
+
+  # Swap amount_borrow for amount_repay on the other router
+  # Vyper does not support "last" indexing using [-1], so use len instead
+  amount_received_after_swap: uint256 = IUniswapV2Router(self.swap_router.address).swapExactTokensForTokens(
+    amount_borrow,
+    amount_repay,
+    self.swap_path,
+    self,
+    block.timestamp + deadline,
+  )[len(self.swap_path)-1]
+
+  # repay the flash borrow to the liquidity pool (msg.sender))
+  # then send the remainder to the transaction originator (tx.origin)
+  if _amount0 > 0:
+    ERC20(token1).transfer(msg.sender, amount_repay)
+    ERC20(token1).transfer(tx.origin, amount_received_after_swap - amount_repay)
+  else:
+    ERC20(token0).transfer(msg.sender, amount_repay)
+    ERC20(token0).transfer(tx.origin, amount_received_after_swap - amount_repay)
---
+++ mev/spell_sspell_arbitrage/scripts/main.py
@@ -0,0 +1,161 @@
+from brownie import *
+import os
+import warnings
+from fractions import Fraction
+from scipy import optimize
+
+# 21141181
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+os.environ["SNOWTRACE_TOKEN"] = "JCGRVAFIADSVISFU675XCRNRNKII4Z7UBJ"
+
+try:
+    network.connect('avax-main')
+except:
+    pass
+
+spell = Contract('0xCE1bFFBD5374Dac86a2893119683F4911a2F7814')
+sspell = Contract('0x3Ee97d514BBef95a2f110e6B9b73824719030f7a')
+# sushi_factory_address = '0xc35DADB65012eC5796536bD9864eD8773aBc74C4'
+sushi_router = Contract('0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506')
+trader_joe_router = Contract('0x60aE616a2155Ee3d9A68541Ba4544862310933d4')
+sushi_pool = Contract('0xE5cddBfd3A807691967e528f1d6b7f00b1919e6F')
+trader_joe_pool = Contract('0x033C3Fc1fC13F803A233D262e24d1ec3fd4EFB48')
+
+
+def two_pool_arbitrage_profit(pool0_reserves, pool1_reserves, borrow_amount, borrow_token0):
+    def getAmountIn(
+            reserves_token0,
+            reserves_token1,
+            fee,
+            token_out_quantity,
+            token0_in
+    ):
+        if token0_in:
+            return int(
+                (reserves_token0 * token_out_quantity)
+                // ((1 - fee) * (reserves_token1 - token_out_quantity))
+                + 1
+            )
+        else:
+            return int(
+                (reserves_token1 * token_out_quantity)
+                // ((1 - fee) * (reserves_token0 - token_out_quantity))
+                + 1
+            )
+
+    def getAmountOut(
+            reserves_token0,
+            reserves_token1,
+            fee,
+            token_in_quantity,
+            token0_in
+    ):
+        if token0_in:
+            return int(reserves_token1 * token_in_quantity * (1 - fee)) // int(
+                reserves_token0 + token_in_quantity * (1 - fee)
+            )
+        else:
+            return int(reserves_token0 * token_in_quantity * (1 - fee)) // int(
+                reserves_token1 + token_in_quantity * (1 - fee)
+            )
+
+    # Borrow pool reserves
+    x0a, y0a = pool0_reserves
+
+    # Swap pool reserves
+    x0b, y0b = pool1_reserves
+
+    # get repayment INPUT at borrow_amount OUTPUT
+    flash_repay_amount = getAmountIn(
+        reserves_token0=x0a,
+        reserves_token1=y0a,
+        fee=Fraction(3, 1000),
+        token_out_quantity=borrow_amount,
+        token0_in=False if borrow_token0 else True  # False
+    )
+
+    swap_amount_out = getAmountOut(
+        reserves_token0=x0b,
+        reserves_token1=y0b,
+        fee=Fraction(3, 1000),
+        token_in_quantity=borrow_amount,
+        token0_in=True if borrow_token0 else False  # True
+    )
+
+    return swap_amount_out - flash_repay_amount
+
+
+def optimize_two_pool_arbitrage(pool0_reserves, pool1_reserves, borrow_token0):
+    y0a = pool0_reserves[1]
+
+    bounds = (1, y0a)
+
+    bracket = (0.01 * y0a, 0.05 * y0a)
+
+    result = optimize.minimize_scalar(
+        lambda x: -float(two_pool_arbitrage_profit(pool0_reserves, pool1_reserves, x, borrow_token0)),
+        method="bounded",
+        bounds=bounds,
+        bracket=bracket
+    )
+
+    return result.x, -result.fun
+
+
+def arbitrage():
+    res = sushi_router.getAmountsOut(10 ** 18, [spell.address, sspell.address])
+    sushi_quote = res[0] / res[1]
+    print('Sushi sSPELL/SPELL:', sushi_quote)
+
+    res = trader_joe_router.getAmountsOut(10 ** 18, [spell.address, sspell.address])
+    trader_joe_quote = res[0] / res[1]
+    print('Trader Joe sPELL/SPELL:', trader_joe_quote)
+
+    # Print reserves
+    sushi_reserves = sushi_pool.getReserves()[:2]
+    trader_joe_reserves = trader_joe_pool.getReserves()[:2]
+
+    print('Sushi reserves', sushi_reserves)
+    print('Trader Joe reserves', trader_joe_reserves)
+
+    # borrow sSPELL if the quote of sSPELL/SPELL in smaller on Sushi
+    if sushi_quote < trader_joe_quote:
+        borrow_token0 = True
+        borrow_token_address = sspell.address
+        swap_path = [sspell.address, spell.address]
+    else:
+        borrow_token0 = False
+        borrow_token_address = spell.address
+        swap_path = [spell.address, sspell.address]
+
+    borrow_amount_opt, profit_opt = optimize_two_pool_arbitrage(sushi_reserves, trader_joe_reserves, borrow_token0)
+    print('Optimal results', borrow_amount_opt / 10 ** 18, profit_opt / 10 ** 18)
+
+    if profit_opt > 0:
+        spell_sspell_arbitrage.deploy(sushi_router.address, {'from': accounts[0]})
+
+        tx = spell_sspell_arbitrage[0].execute(
+            sushi_pool.address,
+            borrow_token_address,
+            borrow_amount_opt,
+            swap_path,
+            trader_joe_router,
+            {'from': accounts[0]}
+        )
+
+        print(tx.info())
+
+        # check profit in balance
+        print('SPELL balance', spell.balanceOf(accounts[0]))
+        print('sPELL balance', sspell.balanceOf(accounts[0]))
+
+
+def main():
+    try:
+        chain.snapshot()
+        print('Snapshot chain at', chain.height)
+        arbitrage()
+    finally:
+        print('Revert chain to', chain.revert())
